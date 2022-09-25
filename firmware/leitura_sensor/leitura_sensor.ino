0e2de5282ec797a61dbe209e3284dc0d1d596300
Charlys Pinheiro <charlys.pinheiro@hp.com>
2022-09-25 15:02:26 -0300

    integrando com a balanca

--- firmware/leitura_sensor/leitura_sensor.ino
+++ firmware/leitura_sensor/leitura_sensor.ino
@@ -1,7 +1,10 @@
+#include "HX711.h"
+#include <MySQL_Generic.h>
+
 #define MYSQL_DEBUG_PORT    Serial
 #define _MYSQL_LOGLEVEL_    1
-
-#include <MySQL_Generic.h>
+#define DOUT 14
+#define SCK 2
 
 IPAddress server(192,168,1,67);   // Ip do servidor de banco de dados
 char ssid[] = "Casa 57_(2.4g)";   // Rede wifi
@@ -10,12 +13,19 @@ char user[] = "root";             // Usuario banco de dados
 char password[] = "crvh1234";     // Senha criada com o usuário
 uint16_t serverPort = 3307;       // Porta do banco MariaDb
 
+HX711 balanca;                    //Define a balanca
+float calibration_factor = 22664; //Define a calibracao
+
 MySQL_Connection conn((Client *)&client);
 
 MySQL_Query *query_mem;
 
 void setup(){
-  Serial.begin(115200);
+  Serial.begin(9600);
+  balanca.begin(DOUT, SCK);
+  balanca.set_scale(calibration_factor);
+  balanca.tare();
+  
   while (!Serial && millis() < 5000);
 
   MYSQL_DISPLAY1("\nInitializing ", ARDUINO_BOARD);
@@ -37,7 +47,10 @@ void dataInsert(){
   MySQL_Query query_mem = MySQL_Query(&conn);
 
   if (conn.connected()){
-    String sql_query = "insert into home_monitor.water(value) values(" + String(12.365) + ");";
+
+    String peso = String(balanca.get_units(),3);
+    
+    String sql_query = "insert into home_monitor.water(value) values(" + peso + ");";
     MYSQL_DISPLAY(sql_query);
 
     if (!query_mem.execute(sql_query.c_str())){
@@ -67,5 +80,5 @@ void loop(){
   MYSQL_DISPLAY("\nAguardando...");
   MYSQL_DISPLAY("===============");
  
-  delay(60000);
+  delay(30000);
 }
